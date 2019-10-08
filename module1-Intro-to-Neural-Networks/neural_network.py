da8a849c2f13726d22dcf1bf2ccf754e37228ecc
Harsh Desai <hrdesai007@gmail.com>
2019-10-08 00:20:12 -0500

    updated perceptron and notebooks for module 1

---
+++ module1-Intro-to-Neural-Networks/neural_network.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+from sklearn.preprocessing import MinMaxScaler, Normalizer
+from sklearn.model_selection import train_test_split
+
+# Layer Dims
+n_hidden = 10
+n_in = 10
+
+# Outputs
+n_out = 10
+
+# Hyperparameters
+learning_rate = 0.01
+momentum = 0.9
+
+# non deterministic seeding
+np.random.seed(0)
+
+
+def main():
+    diabetes = pd.read_csv(
+        'https://raw.githubusercontent.com/ryanleeallred/datasets/master/diabetes.csv')
+    print(diabetes.head())
+
+    features = list(diabetes)[:-1]
+    target = 'Outcome'
+
+    scaler = MinMaxScaler()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+    print("---------- Shape ----------")
+    print(X_train.shape, X_test.shape)
+    print(y_train.shape, y_test.shape)
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+
+if __name__ == "__main__":
+    main()
---
+++ module1-Intro-to-Neural-Networks/perceptron.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Perceptron():
+
+    def __init__(self, input_size=2, output_size=1, lr=0.01, niter=10):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.lr = lr
+        self.niter = niter
+
+        # Initialize weights once
+        self.W = self.__init_weights(type_='custom')
+        self.b = self.__init_bias()
+        self.loss = []
+
+    def __init_weights(self, type_='custom'):
+        if type_ == 'uniform':
+            W = np.random.random(self.input_size, self.output_size)
+        elif type_ == 'normal':
+            W = np.random.randn(self.input_size, self.output_size)
+        elif type_ == 'custom':
+            W = 2 * np.random.random((self.input_size, self.output_size)) - 1
+        else:
+            raise ValueError(
+                'Parameter passed is wrong type, can only be "uniform", "normal", or "custom"')
+        return W
+
+    def __init_bias(self):
+        return np.full((1, self.output_size), 0.1)
+
+    def __sigmoid(self, X):
+        return 1 / (1 + np.exp(-X))
+
+    def __sigmoid_derivative(self, X):
+        sx = self.__sigmoid(X)
+        return sx * (1-sx)
+
+    def __feed_forward(self, X):
+        """
+        Computes Z values, which passing through sigmoid gives prediction probability
+        """
+        # Weighted sum of inputs / weights + bias
+        Z = (X @ self.W) + self.b
+        return Z
+
+    def __backprop(self, dO, X):
+        """
+        Calculate weight adjustments
+        """
+        dW = np.dot(X.T, dO)
+        db = np.sum(dO)
+        return dW, db
+
+    def fit(self, X, y):
+        """Fit training data
+        X : Training vectors, X.shape : [#samples, #features]
+        y : Target values, y.shape : [#samples]
+        """
+        try:
+            y.shape[1]
+        except IndexError:
+            y = y.reshape(-1, 1)
+
+        for i in range(self.niter):
+            # Forward Prop
+            Z = self.__feed_forward(X)
+            y_hat = self.__sigmoid(Z)
+
+            # Calculate cost/error MAE
+            E = (y - y_hat)
+            self.loss.append(np.sum(E.T))
+
+            # Calculate adjustments/gradient
+            dO = E * self.__sigmoid_derivative(y_hat)
+            dW, db = self.__backprop(dO, X)
+
+            # Update the Weights with new gradient
+            self.W += self.lr * dW
+            self.b += self.lr * db
+
+    def predict_proba(self, X):
+        """Return prediction probabilites"""
+        return self.__sigmoid(self.__feed_forward(X))
+
+    def predict(self, X, threshold=0.5):
+        """Return class labels"""
+        pred_probas = self.predict_proba(X)
+        return (pred_probas > threshold).astype('int32')
+
+    def plot_loss(self):
+        plt.figure(figsize=(10, 6))
+        plt.plot(np.arange(1, len(self.loss)+1), np.array(self.loss),
+                 marker='o', color='steelblue')
+        plt.xlabel('Epochs')
+        plt.ylabel('MAE Error')
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    data = {'x1': [0, 1, 0, 1],
+            'x2': [0, 0, 1, 1],
+            'y':  [1, 1, 1, 0]
+            }
+
+    df = pd.DataFrame.from_dict(data).astype('int')
+
+    X = df[['x1', 'x2']].to_numpy()
+    y = df['y'].to_numpy().reshape(-1, 1)
+
+    print('*'*80)
+    print('----- Shape of data ------')
+    print(f'X.shape: {X.shape}')
+    print(f'y.shape: {y.shape}')
+
+    nand = Perceptron(input_size=2, output_size=1, lr=0.01, niter=1000)
+    nand.fit(X, y)
+    print('------- Weights & Biases -------')
+    print('Weights after training')
+    print(nand.W)
+    print('Bias after training')
+    print(nand.b)
+    print('------- NAND Gate -------')
+    # print(f'Predict X1: 0, X2: 0, y = {nand.predict(np.array([0, 0]))}')
+    # print(f'Predict X1: 0, X2: 1, y = {nand.predict(np.array([0, 1]))}')
+    # print(f'Predict X1: 1, X2: 0, y = {nand.predict(np.array([1, 0]))}')
+    # print(f'Predict X1: 1, X2: 1, y = {nand.predict(np.array([1, 1]))}')
+    test_X = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+    print(nand.predict(test_X))
+
+    print('*'*80)
